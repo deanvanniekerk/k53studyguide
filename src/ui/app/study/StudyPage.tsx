6c7070314c29949e2c88de771199e1ac183e7daf
Dean van Niekerk <deanvniekerk@gmail.com>
2020-02-10 19:52:49 +0200

    icons on nav list

--- src/ui/app/study/StudyPage.tsx
+++ src/ui/app/study/StudyPage.tsx
@@ -5,21 +5,19 @@ import { connect } from "react-redux";
 import { STUDY_BACKGROUND_COLORS } from "@/data/branding";
 import { RootState } from "@/state";
 import { currentNavigationItemsSelector } from "@/state/study/navigation";
-import { translationsSelector } from "@/state/translations/selectors";
-import { Text } from "@/ui/components";
 import { Page } from "@/ui/components/Page";
+
+import { NavigationItem } from "./components/NavigationItem";
 
 type Props = PropsFromState;
 
 const StudyPageComponent: React.FC<Props> = props => {
     return (
         <Page backgroundColors={STUDY_BACKGROUND_COLORS}>
-            <View
-                style={{ flex: 1, width: "100%", alignItems: "center", justifyContent: "center" }}
-            >
-                {props.navigationItems.map(n => {
-                    return <Text key={n}>{props.translations[n]}</Text>;
-                })}
+            <View>
+                {props.navigationItems.map(n => (
+                    <NavigationItem key={n} navigationItemKey={n} />
+                ))}
             </View>
         </Page>
     );
@@ -28,11 +26,9 @@ const StudyPageComponent: React.FC<Props> = props => {
 type PropsFromState = ReturnType<typeof mapStateToProps>;
 const mapStateToProps = (state: RootState) => {
     const navigationItems = currentNavigationItemsSelector(state);
-    const translations = translationsSelector(state);
 
     return {
         navigationItems,
-        translations,
     };
 };
 
--- src/ui/app/study/components/NavigationItem.tsx
+++ src/ui/app/study/components/NavigationItem.tsx
@@ -5,6 +5,7 @@ import { connect } from "react-redux";
 import { RootState } from "@/state";
 import { translationsSelector } from "@/state/translations/selectors";
 import { Text } from "@/ui/components";
+import { FontAwesome } from "@expo/vector-icons";
 
 type Props = {
     navigationItemKey: string;
@@ -12,8 +13,35 @@ type Props = {
 
 const NavigationItemComponent: React.FC<Props> = props => {
     return (
-        <View style={{ flex: 1, width: "100%", alignItems: "center", justifyContent: "center" }}>
-            <Text>{props.translations[props.navigationItemKey]}</Text>
+        <View
+            style={{
+                flexDirection: "row",
+                alignItems: "stretch",
+                height: 50,
+                borderWidth: 1,
+            }}
+        >
+            <View style={{ justifyContent: "center", borderWidth: 1, borderColor: "red" }}>
+                <FontAwesome
+                    style={{ marginLeft: 10, marginRight: 10 }}
+                    name="user-circle"
+                    size={16}
+                    color={"#FFFFFF"}
+                />
+            </View>
+            <View
+                style={{ flex: 1, justifyContent: "center", borderWidth: 1, borderColor: "green" }}
+            >
+                <Text>{props.translations[props.navigationItemKey]}</Text>
+            </View>
+            <View style={{ justifyContent: "center", borderWidth: 1, borderColor: "red" }}>
+                <FontAwesome
+                    style={{ marginLeft: 10, marginRight: 10 }}
+                    name="user-circle"
+                    size={16}
+                    color={"#FFFFFF"}
+                />
+            </View>
         </View>
     );
 };
