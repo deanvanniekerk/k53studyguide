541418d19312893f72e55d2bef58cb0dbd7f262c
Dean van Niekerk <deanvniekerk@gmail.com>
2020-02-13 20:55:54 +0200

    updated package versions

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import "./Tab1.css";
+
+import React from "react";
+
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
+
+import ExploreContainer from "../components/ExploreContainer";
 
 const Tab1: React.FC = () => {
   return (
@@ -17,7 +20,7 @@ const Tab1: React.FC = () => {
             <IonTitle size="large">Tab 1</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+        <ExploreContainer name="Tab 1 page xx" />
       </IonContent>
     </IonPage>
   );
