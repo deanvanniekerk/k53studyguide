b09373c25e4e6c48043ebb3cb9f5c1faace4306f
Dean van Niekerk <deanvniekerk@gmail.com>
2020-02-27 15:26:44 +0200

    added translations

--- src/data/types.ts
+++ src/data/types.ts
@@ -18,6 +18,9 @@ export type Translations = {
 
 export type Translation = {
     en: string;
+    af: string;
+    zu: string;
+    xh: string;
 };
 
 export type QuestionData = {
---
+++ tools/translator/main.js
@@ -0,0 +1,75 @@
+const fs = require('fs');
+const { translations } = require("./input/translations");
+
+const key = '***REMOVED_GOOGLE_API_KEY***';
+
+const { Translate } = require('@google-cloud/translate').v2;
+
+const translate = new Translate({ key: key });
+
+const crunch = async () => {
+
+    const keys = Object.keys(translations);
+
+    for (let i = 0; i < keys.length; i++) {
+        const key = keys[i];
+
+        const translation = translations[key];
+
+
+        console.log(``);
+        console.log(`=================================`);
+
+        console.log(`key: ${key}`);
+
+        console.log(`---------------------------------`);
+
+
+
+        try {
+
+            let t = "";
+
+            //Afrikaans 
+            if (!translation.af) {
+                [t] = await translate.translate(translation.en, { from: 'en', to: 'af' });
+                console.log(`af: ${t.substring(0, Math.min(20, t.length - 1))}`);
+                translation.af = t;
+            }
+
+            //Zulu 
+            if (!translation.zu) {
+                [t] = await translate.translate(translation.en, { from: 'en', to: 'zu' });
+                console.log(`zu: ${t.substring(0, Math.min(20, t.length - 1))}`);
+                translation.zu = t;
+            }
+
+            //Xhosa 
+            if (!translation.xh) {
+                [t] = await translate.translate(translation.en, { from: 'en', to: 'xh' });
+                console.log(`xh: ${t.substring(0, Math.min(20, t.length - 1))}`);
+                translation.xh = t;
+            }
+
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    fs.writeFileSync('./output/translations.ts', `
+import { Translations } from "./";
+
+const translations: Translations = ${JSON.stringify(translations, null, 4)}
+
+export { translations };
+`);
+}
+
+crunch();
+
+
+
+
+
+
