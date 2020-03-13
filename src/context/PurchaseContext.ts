125d666c930624e1e88a3b06c6ce221892feccce
Dean van Niekerk <deanvniekerk@gmail.com>
2020-03-13 15:04:46 +0200

    fixed compile issue

--- src/context/PurchaseContext.ts
+++ src/context/PurchaseContext.ts
@@ -1,5 +1,5 @@
 import React from "react";
 
-import { PurchaseService } from "@/purchase";
+import { PurchaseService } from "@/services";
 
 export const PurchaseContext = React.createContext<PurchaseService | null>(null);
