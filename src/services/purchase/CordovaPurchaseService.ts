d23b07010f85a216030e1d7761b6dd2e36cc9a9e
Dean van Niekerk <deanvniekerk@gmail.com>
2020-03-14 15:29:31 +0200

    updates to purchase service

--- src/services/purchase/CordovaPurchaseService.ts
+++ src/services/purchase/CordovaPurchaseService.ts
@@ -21,69 +21,60 @@ export class CordovaPurchaseService implements PurchaseService {
 
         InAppPurchase2.verbosity = InAppPurchase2.DEBUG;
 
-        InAppPurchase2.ready(() => {
-            this.log("CordovaPurchaseService > ready");
-
-            //Register
-            InAppPurchase2.register({
-                id: this._productId,
-                type: InAppPurchase2.NON_RENEWING_SUBSCRIPTION,
-            });
-
-            //Subscribe to any additional changes
-            InAppPurchase2.when(this._productId).updated(this.handleProductChange);
-
-            //Refresh
-            this.refresh();
-
-            //Initial load of product
-            this._product = InAppPurchase2.get(this._productId);
-            this.handleProductChange(this._product);
-
-            InAppPurchase2.when(this._product).approved((product: IAPProduct) => {
-                this.log("CordovaPurchaseService > product approved");
-                this.handleProductChange(product);
-            });
+        //Register
+        InAppPurchase2.register({
+            id: this._productId,
+            type: InAppPurchase2.NON_RENEWING_SUBSCRIPTION,
         });
+
+        //Subscribe to any additional changes
+        InAppPurchase2.when(this._productId).updated((product: IAPProduct) => {
+            this.log("CordovaPurchaseService > product changed", {
+                product: JSON.stringify(product, null, 4),
+            });
+
+            if (product.state === InAppPurchase2.APPROVED) {
+                product.finish();
+            }
+
+            const action = recievePurchaseStatus(
+                product.owned,
+                product.canPurchase,
+                product.expiryDate,
+                product.state,
+                product.price,
+                product.title,
+                product.description
+            );
+
+            this._reduxStore.dispatch(action);
+        });
+
+        //Initial load of product
+        //this._product = InAppPurchase2.get(this._productId);
+        //this.handleProductChange(this._product);
+
+        // InAppPurchase2.when(this._product).approved((product: IAPProduct) => {
+        //     this.log("CordovaPurchaseService > product approved");
+        //     this.handleProductChange(product);
+        // });
 
         InAppPurchase2.error((error: unknown) => {
-            this.log("CordovaPurchaseService > Error");
             this.log("CordovaPurchaseService > Error : " + JSON.stringify(error));
         });
+
+        //Refresh
+        this.refresh();
     }
 
     refresh() {
         InAppPurchase2.refresh();
     }
 
-    handleProductChange(product: IAPProduct) {
-        this.log("CordovaPurchaseService > product changed", {
-            product: JSON.stringify(product, null, 4),
-        });
-
-        if (product.state === InAppPurchase2.APPROVED) {
-            product.finish();
-        }
-
-        const action = recievePurchaseStatus(
-            product.owned,
-            product.canPurchase,
-            product.expiryDate,
-            product.state,
-            product.price,
-            product.title,
-            product.description
-        );
-
-        this._reduxStore.dispatch(action);
-    }
-
     purchase() {
-        if (!this._product) return;
-
         this.log("CordovaPurchaseService > ordering product");
 
-        InAppPurchase2.order(this._product);
+        InAppPurchase2.order(this._productId);
     }
 
     log(message: string, data?: LogData) {
