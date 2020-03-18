eaed4f815e6afb6bbc94bb9d7f94bd57b680ba3a
Dean van Niekerk <deanvniekerk@gmail.com>
2020-03-18 09:57:02 +0200

    added additional debug info

--- src/app/pages/profile/components/Debug.tsx
+++ src/app/pages/profile/components/Debug.tsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 
 import { HorizontalRule } from "@/app/components";
+import { AppVersion } from "@ionic-native/app-version";
 import { Device } from "@ionic-native/device";
 import { IonCol, IonGrid, IonRow, IonText } from "@ionic/react";
 
 import { Row } from "./";
 
 const Debug: React.FC = () => {
+    const [appVersionNumber, setAppVersionNumber] = useState("");
+    const [appVersionCode, setAppVersionCode] = useState("");
+    const [appPackageName, setAppPackageName] = useState("");
+
+    useEffect(() => {
+        const load = async () => {
+            const avn = await AppVersion.getVersionNumber();
+            setAppVersionNumber(avn);
+
+            const avc = await AppVersion.getVersionCode();
+            setAppVersionCode(avc.toString());
+
+            const apn = await AppVersion.getPackageName();
+            setAppPackageName(apn);
+        };
+        load();
+    }, []);
+
     return (
         <Grid>
             <FullRow>
@@ -15,6 +34,9 @@ const Debug: React.FC = () => {
                     <Title>Debug</Title>
                 </TitleCol>
             </FullRow>
+            <Row name="App Version Number" value={appVersionNumber} />
+            <Row name="App Version Code" value={appVersionCode} />
+            <Row name="App Package Name" value={appPackageName} />
             <Row name="Device Model" value={Device.model} />
             <Row name="Device Id" value={Device.uuid} />
             <Row name="Device Version" value={Device.version} />
