0f70e9fcb39263732fca685e6c18e4eb6b467f41
Dean van Niekerk <deanvniekerk@gmail.com>
2021-07-27 17:28:28 +0200

    fix app rate (take 2)

--- src/app/Startup.tsx
+++ src/app/Startup.tsx
@@ -14,22 +14,36 @@ const Startup: React.FC = () => {
             purchaseService.initialize();
         }
 
-        //https://github.com/pushandplay/cordova-plugin-apprate
-        appRate.preferences = {
-            ...appRate.preferences,
-            simpleMode: true,
-            storeAppURL: {
-                android: "market://details?id=deanvniekerk.k53studyguide.app",
-                ios: "1503354808",
-            },
-            customLocale: {
-                title: "Would you mind rating K53 Study Guide?",
-                message: "Any feedback would be greatly appreciated. Thank you for your support!",
-                cancelButtonLabel: "No thanks",
-                laterButtonLabel: "Remind me later",
-                rateButtonLabel: "Rate it Now",
-            },
+        const preferences = appRate.getPreferences();
+        preferences.simpleMode = true;
+        preferences.customLocale = {
+            title: "Would you mind rating K53 Study Guide?",
+            message: "Any feedback would be greatly appreciated. Thank you for your support!",
+            cancelButtonLabel: "No thanks",
+            laterButtonLabel: "Remind me later",
+            rateButtonLabel: "Rate it Now",
         };
+        preferences.storeAppURL = {
+            android: "market://details?id=deanvniekerk.k53studyguide.app",
+        };
+        appRate.setPreferences(preferences);
+
+        //https://github.com/pushandplay/cordova-plugin-apprate
+        // appRate.preferences = {
+        //     ...appRate.preferences,
+        //     simpleMode: true,
+        //     storeAppURL: {
+        //         android: "market://details?id=deanvniekerk.k53studyguide.app",
+        //         ios: "1503354808",
+        //     },
+        //     customLocale: {
+        //         title: "Would you mind rating K53 Study Guide?",
+        //         message: "Any feedback would be greatly appreciated. Thank you for your support!",
+        //         cancelButtonLabel: "No thanks",
+        //         laterButtonLabel: "Remind me later",
+        //         rateButtonLabel: "Rate it Now",
+        //     },
+        // };
 
         setTimeout(() => {
             appRate.promptForRating(false);
--- src/app/hooks/useAppRate.ts
+++ src/app/hooks/useAppRate.ts
@@ -1,5 +1,6 @@
 import { getAppRate } from "@/services/appRate";
 
+// needed because: https://github.com/pushandplay/cordova-plugin-apprate/issues/291
 export const useAppRate = () => {
     const appRate = getAppRate();
     return appRate;
